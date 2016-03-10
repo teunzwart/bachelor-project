a58145c678c7e514d12aee53a22dce1127726651
teunzwart <teunzwart@users.noreply.github.com>
2016-03-10 22:41:56 +0100

    Write parser, start IsingModel class

--- code/ising.py
+++ code/ising.py
@@ -1,15 +1,61 @@
-
 import argparse
 import json
 import logging
+import time
 
-def argument_parser():
-    """Parse command line arguments."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--debug")
+import matplotlib.pyplot as plt
+
+
+class IsingModel:
+    """
+
+    """
+
+    def __init__(self, xsize, ysize, debug):
+        """
+        Initialize Ising model simulation.
+
+        Args:
+            xsize: Extent of simulation in x-direction.
+            ysize: Extent of simulation in y-direction.
+            debug: Whether debugging info has to be shown.
+        """
+        if debug:
+            logging.basicConfig(
+                format="%(levelname)s:%(message)s",
+                level=logging.DEBUG)
+        self.xsize = xsize
+        self.ysize = ysize
+        self.start_time = time.time()
+        self.start_time_human = 0  #TODO: Something todo
+        logging.info("Simulation started on {0}".format(time.strftime("%a, %d %b %Y %H:%M:%S, time.localtime())))
+
+
+def _argument_parser():
+    """
+    Parse command line arguments.
+
+    Returns:
+        Namespace containing argumentsself.
+    """
+    parser = argparse.ArgumentParser(
+        description="Simulate the Ising model in 2D.")
+    parser.add_argument(
+        "xsize",
+        help="specify x extent",
+        type=int)
+    parser.add_argument(
+        "ysize",
+        help="specify y extent",
+        type=int)
+    parser.add_argument(
+        "-d", "--debug",
+        help="print debugging information",
+        action="store_true")
     arguments = parser.parse_args()
     return arguments
 
 if __name__ == "__main__":
-    arguments = argument_parser()
+    arguments = _argument_parser()
     print(arguments)
+    ising = IsingModel(arguments.xsize, arguments.ysize, arguments.debug)
