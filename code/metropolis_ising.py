a072d60bb8d33e8f0cd9539e304c51157f0018fb
teunzwart <teunzwart@users.noreply.github.com>
2016-04-08 17:13:02 +0200

    Add exact solutions and anti-ferromagnetic ground state

--- code/metropolis_ising.py
+++ code/metropolis_ising.py
@@ -1,6 +1,7 @@
 """An implementation of the Metropolis algorithm for the Ising model."""
 
 import numpy as np
+import scipy.special
 
 
 class MetropolisIsing:
@@ -10,6 +11,7 @@ class MetropolisIsing:
                  initial_temperature, sweeps):
         """Initialize variables and the lattice."""
         self.lattice_size_L = lattice_size_L
+        self.no_of_sites = lattice_size_L**2
         self.bond_energy_J = bond_energy_J
         self.temperature_T = temperature_T
         self.beta = 1 / self.temperature_T
@@ -18,31 +20,34 @@ class MetropolisIsing:
         self.lattice = self.init_lattice()
         self.energy = self.calculate_lattice_energy()
         self.exponents = self.exponents_init()
-        self.energy_history = []
-        self.magnet_history = []
+        self.energy_history = np.empty(self.sweeps)
+        self.magnet_history = np.empty(self.sweeps)
 
     def init_lattice(self):
         """
         Initialize the lattice for the given initial temperature.
 
         Broken symmetry in the ferromagnetic case (J>0) is taken into account.
-        Anti-ferromagnetic ground state is not implented (alternatly positive and negative orientation).
+        Anti-ferromagnetic ground state has alternatly positive and negative orientation.
         "hi" corresponds to infinte temperature, "lo" to T=0.
         """
         if self.initial_temperature == "hi":
-            lattice = np.random.choice([-1, 1], self.lattice_size_L**2).reshape(self.lattice_size_L, self.lattice_size_L)
+            lattice = np.random.choice([-1, 1], self.no_of_sites).reshape(self.lattice_size_L, self.lattice_size_L)
 
         elif self.initial_temperature == "lo":
             if self.bond_energy_J > 0:
                 # Broken ground state energy.
                 ground_state = np.random.choice([-1, 1])
+                lattice = np.full((self.lattice_size_L, self.lattice_size_L), ground_state, dtype="int64")
             elif self.bond_energy_J < 0:
-                # TODO: Implement anti-ferromagnetic ground state.
-                raise NotImplementedError("Anti-ferromagnetic ground state not implemented")
+                # Set lattice to alternating pattern.
+                row1 = np.hstack([1, -1] * self.lattice_size_L)
+                row2 = np.hstack([-1, 1] * self.lattice_size_L)
+                lattice = np.vstack([row1, row2] * self.lattice_size_L)
             else:
                 raise Exception("Bond energy can not be 0.")
 
-            lattice = np.full((self.lattice_size_L, self.lattice_size_L), ground_state, dtype="int64")
+
         return lattice
 
     def calculate_lattice_energy(self):
@@ -78,8 +83,8 @@ class MetropolisIsing:
             if t % (self.sweeps / 10) == 0:
                 print("Sweep {0}".format(t))
             # Measurement every sweep.
-            self.energy_history.append(self.energy)
-            self.magnet_history.append(np.sum(self.lattice))
+            np.put(self.energy_history, t, self.energy)
+            np.put(self.magnet_history, t, np.sum(self.lattice))
             for k in range(self.lattice_size_L ** 2):
                 # Pick a random location on the lattice.
                 rand_y = np.random.randint(0, self.lattice_size_L)
@@ -109,6 +114,21 @@ class MetropolisIsing:
                 self.lattice[rand_y, rand_x] = -1 * spin
                 self.energy += energy_delta
 
+    def exact_magnetization(self, temperature):
+        """Calculate the exact magnetization. Boltzmann constant is set to 1."""
+        M = (1 - np.sinh((2 / temperature) * self.bond_energy_J)**(-4))**(1 / 8)
+        if np.isnan(M):
+            return 0
+        return M
+
+    def exact_energy(self, temperature):
+        """Calculate the exact energy with Boltzmann constant set to 1."""
+        k = 2 * np.sinh((2 / temperature) * self.bond_energy_J) / (np.cosh((2 / temperature) * self.bond_energy_J))**2
+        energy = (- self.bond_energy_J * (1 / np.tanh((2 / temperature) * self.bond_energy_J)) *
+                  (1 + (2 / np.pi) * (2 * np.tanh((2 / temperature) * self.bond_energy_J)**2 - 1) *
+                   scipy.special.ellipk(k)))
+
+        return energy
 
 if __name__ == "__main__":
     metropolis_ising = MetropolisIsing(4, 1, 2, "lo", 1000)
