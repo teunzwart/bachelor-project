b776345617e877562de928c197eddc72ef44e5b2
teunzwart <teunzwart@users.noreply.github.com>
2016-04-28 11:46:00 +0200

    Change data structure for data points
    Changed to (value, error)

--- code/plotting.py
+++ code/plotting.py
@@ -53,17 +53,22 @@ def plot_correlation_time_range(data_range, lattice_sizes, quantity, show_plot=T
     for p in range(len(lattice_sizes)):
         plt.plot([d[0] for d in data_range[p]], [d[1] for d in data_range[p]], marker='o', linestyle='None', label="{0} by {0} Lattice".format(lattice_sizes[p]))
     plt.legend(loc='best')
+    # We put all data together so it is easy to find the maximum and minimum values.
+    zipped_data = list(itertools.chain(*data_range))
+    max_x = max(zipped_data, key=lambda x: x[0])[0]
+    plt.xlim(0, max_x + 0.2)
+    data_max = max(zipped_data, key=lambda x: x[1])[1]
+    plt.ylim(0, data_max * 1.15)
     if save:
         plt.savefig("{0}{1}_Autocorrelation_Time_{2}.pdf".format(SAVE_LOCATION, quantity.replace(" ", "_"), time.strftime("%Y%m%d%H%M%S", time.localtime(time.time()))), bbox_inches='tight')
     if show_plot:
         plt.show()
 
 
-def plot_quantity_range(data_range, errors_range, lattice_sizes, quantity, exact=None, show_plot=True, save=False):
+def plot_quantity_range(data_range, lattice_sizes, quantity, exact=None, show_plot=True, save=False):
     """Plot quantity over a temperature range."""
     for p in range(len(lattice_sizes)):
-        plt.plot([d[0] for d in data_range[p]], [d[1] for d in data_range[p]], label="{0} by {0} Lattice".format(lattice_sizes[p]), linestyle='None', marker='o')
-        plt.errorbar([d[0] for d in data_range[p]], [d[1] for d in data_range[p]], [e[1] for e in errors_range[p]], linestyle='None')
+        plt.errorbar([d[0] for d in data_range[p]], [d[1] for d in data_range[p]], [d[2] for d in data_range[p]], linestyle='None', label="{0} by {0} Lattice".format(lattice_sizes[p]), marker='o')
     if exact is not None:
         plt.plot([e[0] for e in exact], [e[1] for e in exact], label="Thermodynamic Limit")
 
--- code/simulation.py
+++ code/simulation.py
@@ -8,34 +8,24 @@ import plotting
 
 
 def temperature_range(model, algorithm, lattice_size, bond_energy, initial_temperature,
-                      thermalization_sweeps, sweeps, lower, upper, step=0.2):
+                      thermalization_sweeps, sweeps, lower, upper, step=0.2, show_values=False):
     """Run a given model over a range of temperature."""
     total_no_of_sweeps = thermalization_sweeps + sweeps
     energy_range = []
-    energy_error_range = []
     energy_correlation_range = []
 
     energy_sq_range = []
-    energy_sq_error_range = []
 
     m_range = []
-    m_error_range = []
     m_correlation_range = []
 
     mag_range = []
-    mag_error_range = []
-
     mag_sq_range = []
-    mag_sq_error_range = []
 
     heat_cap_range = []
-    heat_cap_error_range = []
 
     for t in np.arange(lower, upper, step):
         print("Lattice Size {0}, Temperature {1}".format(lattice_size, round(t, 3)))
-        if t == upper:
-            #TODO: FIX this.
-            print("\n")
         simulation = model(lattice_size, bond_energy, t, initial_temperature, total_no_of_sweeps)
         if algorithm == "metropolis":
             simulation.metropolis()
@@ -50,7 +40,7 @@ def temperature_range(model, algorithm, lattice_size, bond_energy, initial_tempe
         equilibrium_magnetization = simulation.magnetization_history[thermalization_sweeps:] / simulation.no_of_sites
 
         energy, energy_error, energy_correlation, _ = analysis.binning(equilibrium_energy, 10, "Energy per Site")
-        energy_sq, energy_error_sq, energy_sq_correlation, _ = analysis.binning((simulation.no_of_sites * equilibrium_energy)**2, 10, "Energy Squared")
+        energy_sq, energy_sq_error, energy_sq_correlation, _ = analysis.binning((simulation.no_of_sites * equilibrium_energy)**2, 10, "Energy Squared")
 
         m, m_error, m_correlation, _ = analysis.binning(np.absolute(equilibrium_magnetization), 10, "<|M|>/N")
         mag, mag_error, mag_correlation, _ = analysis.binning(equilibrium_magnetization, 10, "Magnetization per Site")
@@ -61,106 +51,90 @@ def temperature_range(model, algorithm, lattice_size, bond_energy, initial_tempe
         heat_cap = np.mean([heat_cap_jack, heat_cap_boot])
         heat_cap_error = max(heat_cap_error_jack, heat_cap_error_jack)
 
-        energy_range.append((t, energy))
-        energy_error_range.append((t, energy_error))
+        energy_range.append((t, energy, energy_error))
         energy_correlation_range.append((t, energy_correlation))
 
-        energy_sq_range.append((t, energy_sq))
-        energy_sq_error_range.append((t, energy_error_sq))
+        energy_sq_range.append((t, energy_sq, energy_sq_error))
 
-        m_range.append((t, m))
-        m_error_range.append((t, m_error))
+        m_range.append((t, m, m_error))
         m_correlation_range.append((t, m_correlation))
 
-        mag_range.append((t, mag))
-        mag_error_range.append((t, mag_error))
+        mag_range.append((t, mag, mag_error))
+        mag_sq_range.append((t, mag_sq, mag_sq_error))
 
-        mag_sq_range.append((t, mag_sq))
-        mag_sq_error_range.append((t, mag_sq_error))
+        heat_cap_range.append((t, heat_cap, heat_cap_error))
 
-        heat_cap_range.append((t, heat_cap))
-        heat_cap_error_range.append((t, heat_cap_error))
+        if show_values:
+            print("Energy per Site: {0} +/- {1}".format(energy, energy_error))
+            print("Magnetization per Site: {0} +/- {1}".format(mag, mag_error))
+            print("m (<|M|>/N): {0} +/- {1}".format(m, m_error))
+            print("Energy Squared: {0} +/- {1}".format(energy_sq, energy_sq_error))
+            print("Magnetization Squared: {0} +/- {1}".format(mag_sq, mag_sq_error))
+            print("Heat Capacity per Site: {0} +/- {1}".format(heat_cap, heat_cap_error))
+            print("\n")
 
-    return (energy_range, energy_error_range, energy_correlation_range,
-            energy_sq_range, energy_sq_error_range,
-            m_range, m_error_range, m_correlation_range,
-            mag_range, mag_error_range,
-            mag_sq_range, mag_sq_error_range,
-            heat_cap_range, heat_cap_error_range)
+    return (energy_range, energy_correlation_range,
+            energy_sq_range,
+            m_range, m_correlation_range,
+            mag_range,
+            mag_sq_range,
+            heat_cap_range)
 
 
 def lattice_size_range(lattice_sizes, model, algorithm, bond_energy,
                        initial_temperature, thermalization_sweeps, sweeps, lower,
-                       upper, step=0.2, save_plots=False):
+                       upper, step=0.2, save_plots=False, show_values=False):
     energy_lattice_range = []
-    energy_error_lattice_range = []
     energy_correlation_lattice_range = []
 
     energy_sq_lattice_range = []
-    energy_sq_error_lattice_range = []
 
     m_lattice_range = []
-    m_error_lattice_range = []
     m_correlation_lattice_range = []
 
     mag_lattice_range = []
-    mag_error_lattice_range = []
 
     mag_sq_lattice_range = []
-    mag_sq_error_lattice_range = []
 
     heat_cap_lattice_range = []
-    heat_cap_error_lattice_range = []
 
     for p in range(len(lattice_sizes)):
-        (energy_range, energy_error_range, energy_correlation_range,
-         energy_sq_range, energy_sq_error_range,
-         m_range, m_error_range, m_correlation_range,
-         mag_range, mag_error_range,
-         mag_sq_range, mag_sq_error_range,
-         heat_cap_range, heat_cap_error_range) = temperature_range(model, algorithm,
-                                                                   lattice_sizes[p],
-                                                                   bond_energy,
-                                                                   initial_temperature,
-                                                                   thermalization_sweeps,
-                                                                   sweeps, lower,
-                                                                   upper, step)
+        (energy_range, energy_correlation_range, energy_sq_range,
+         m_range, m_correlation_range,
+         mag_range, mag_sq_range,
+         heat_cap_range) = temperature_range(model, algorithm, lattice_sizes[p],
+                                             bond_energy, initial_temperature,
+                                             thermalization_sweeps, sweeps,
+                                             lower, upper, step, show_values)
+
         energy_lattice_range.append(energy_range)
-        energy_error_lattice_range.append(energy_error_range)
         energy_correlation_lattice_range.append(energy_correlation_range)
 
         energy_sq_lattice_range.append(energy_sq_range)
-        energy_sq_error_lattice_range.append(energy_sq_error_range)
 
         m_lattice_range.append(m_range)
-        m_error_lattice_range.append(m_error_range)
         m_correlation_lattice_range.append(m_correlation_range)
 
         mag_lattice_range.append(mag_range)
-        mag_error_lattice_range.append(mag_error_range)
-
         mag_sq_lattice_range.append(mag_sq_range)
-        mag_sq_error_lattice_range.append(mag_sq_error_range)
 
         heat_cap_lattice_range.append(heat_cap_range)
-        heat_cap_error_lattice_range.append(heat_cap_error_range)
 
-    exact_energy = exact.internal_energy(bond_energy, lower, upper)
-    exact_heat_capacity = exact.heat_capacity(bond_energy, lower, upper)
-    exact_magnetization = exact.magnetization(bond_energy, lower, upper)
+    exact_energy = exact.internal_energy(bond_energy, 0.01, upper)
+    exact_heat_capacity = exact.heat_capacity(bond_energy, 0.01, upper)
+    exact_magnetization = exact.magnetization(bond_energy, 0.01, upper)
 
-    plotting.plot_quantity_range(energy_lattice_range, energy_error_lattice_range, lattice_sizes, "Energy per Site", exact=exact_energy, save=save_plots)
+    plotting.plot_quantity_range(energy_lattice_range, lattice_sizes, "Energy per Site", exact=exact_energy, save=save_plots)
     plotting.plot_correlation_time_range(energy_correlation_lattice_range, lattice_sizes, "Energy per Site", save=save_plots)
-    plotting.plot_quantity_range(energy_sq_lattice_range, energy_sq_error_lattice_range, lattice_sizes, "Energy Squared", save=save_plots)
+    plotting.plot_quantity_range(energy_sq_lattice_range, lattice_sizes, "Energy Squared", save=save_plots)
 
-    plotting.plot_quantity_range(m_lattice_range, m_error_lattice_range, lattice_sizes, "Absolute Magnetization per Site", exact=exact_magnetization, save=save_plots)
-    plotting.plot_quantity_range(mag_sq_lattice_range, mag_sq_error_lattice_range, lattice_sizes, "Squared Magnetization per Site", save=save_plots)
+    plotting.plot_quantity_range(m_lattice_range, lattice_sizes, "Absolute Magnetization per Site", exact=exact_magnetization, save=save_plots)
+    plotting.plot_quantity_range(mag_sq_lattice_range, lattice_sizes, "Squared Magnetization per Site", save=save_plots)
 
-    plotting.plot_quantity_range(heat_cap_lattice_range, heat_cap_error_lattice_range, lattice_sizes, "Heat Capacity per Site", exact=exact_heat_capacity, save=save_plots)
+    plotting.plot_quantity_range(heat_cap_lattice_range, lattice_sizes, "Heat Capacity per Site", exact=exact_heat_capacity, save=save_plots)
 
-    return (energy_lattice_range, energy_error_lattice_range, energy_correlation_lattice_range,
-            energy_sq_lattice_range, energy_sq_error_lattice_range,
-            m_lattice_range, m_error_lattice_range, m_correlation_lattice_range,
-            mag_lattice_range, mag_error_lattice_range,
-            mag_sq_lattice_range, mag_sq_error_lattice_range,
-            heat_cap_lattice_range, heat_cap_error_lattice_range)
+    return (energy_lattice_range, energy_correlation_lattice_range,
+            energy_sq_lattice_range,
+            m_lattice_range, m_correlation_lattice_range,
+            mag_lattice_range, mag_sq_lattice_range,
+            heat_cap_lattice_range)
